f2da22703c1419f40345e1f5244c50c89e83b572
General Failure <gf59ru@live.com>
2015-03-27 11:47:43 +0500

    contacts app helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) {'Ruby on Rails Tutorial Sample App'}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +11,11 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -20,7 +26,7 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -31,7 +37,18 @@ describe "Static Pages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Контакты" do
+    it 'Должны содержать контент «Контакты»' do
+      visit '/static_pages/contacts'
+      expect(page).to have_content('Контакты')
+    end
+    it 'Должны иметь правильный заголовок' do
+      visit '/static_pages/contacts'
+      expect(page).to have_title("#{base_title} | Контакты")
     end
   end
 
